f82ef5dac3cb722f937667a5139e205adca9de3d
Vasco Costa <vasco.costa@gmail.com>
2017-11-07 11:47:43 +0000

    add initial support for Amiga and Mac files.

--- engines/dgds/dgds.cpp
+++ engines/dgds/dgds.cpp
@@ -86,7 +86,7 @@ struct DgdsChunk {
 	uint32 chunkSize;
 	bool container;
 	
-	bool readHeader(DgdsFileCtx& ctx, Common::File& archive);
+	int readHeader(DgdsFileCtx& ctx, Common::File& archive, const char *name);
 	bool isSection(const Common::String& section);
 
 	bool isPacked(const Common::String& ext);	
@@ -98,12 +98,36 @@ bool DgdsChunk::isSection(const Common::String& section) {
        return section.equals(type);
 }
 
+bool isCompact(const Common::String& ext) {
+	bool compact = false;
+
+	if (0) {}/*
+	else if (ext.equals("BMP"))
+		compact = true;*/
+	else if (ext.equals("AMG"))
+		compact = true;
+	else if (ext.equals("INS"))
+		compact = true;
+	else if (ext.equals("RST"))
+		compact = true;
+	else if (ext.equals("VIN"))
+		compact = true;
+	/* SOME SNG have subheaders SNG:SSM|SNG|FCU(?)|INF|DIG */
+	/* SOUNDS.SNG is particularly large. */
+	/* check for .SCR, .SNG */
+	return compact;
+}
+
 bool DgdsChunk::isPacked(const Common::String& ext) {
 	bool packed = false;
 
 	if (0) {
 	}
 	else if (ext.equals("ADS")) {
+		if (0) {
+		} else if (strcmp(type, "SCR:") == 0) packed = true;
+	}
+	else if (ext.equals("ADH")) {
 		if (0) {
 		} else if (strcmp(type, "SCR:") == 0) packed = true;
 	}
@@ -155,23 +179,25 @@ bool DgdsChunk::isPacked(const Common::String& ext) {
 	else if (ext.equals("TDS")) {
 		if (strcmp(type, "TDS:") == 0) packed = true;
 	}
-
 	else if (ext.equals("TTM")) {
 		if (strcmp(type, "TT3:") == 0) packed = true;
 	}
 	return packed;
 }
 
-bool DgdsChunk::readHeader(DgdsFileCtx& ctx, Common::File& archive) {
+int DgdsChunk::readHeader(DgdsFileCtx& ctx, Common::File& archive, const char *name) {
 	if (ctx.bytesRead >= ctx.inSize) {
-		return false;
+		return 0;
 	}
 	
 	archive.read(type, DGDS_TYPENAME_MAX);
 
 	if (type[DGDS_TYPENAME_MAX-1] != ':') {
-		ctx.outSize = ctx.inSize;
-		return false;
+	    debug("bad header in: %s", name);
+		type[0] = '\0';
+	ctx.bytesRead += DGDS_TYPENAME_MAX;
+	ctx.outSize += DGDS_TYPENAME_MAX;
+		return 1;
 	}
 	type[DGDS_TYPENAME_MAX] = '\0';
 
@@ -188,7 +214,7 @@ bool DgdsChunk::readHeader(DgdsFileCtx& ctx, Common::File& archive) {
 
 	ctx.bytesRead += 4;
 	ctx.outSize += 4;
-	return true;
+	return 2;
 }
  
 Common::SeekableReadStream* DgdsChunk::decode(DgdsFileCtx& ctx, Common::File& archive) {
@@ -348,127 +374,203 @@ static void explode(const char *indexName, bool save) {
 				
 				const char *ext;
 
-				ext = name + strlen(name) - 3;
-
-				struct DgdsChunk chunk;
-				while (chunk.readHeader(ctx, archive)) {
-					bool packed = chunk.isPacked(ext);
-
-					Common::SeekableReadStream *stream;
-					stream = packed ? chunk.decode(ctx, archive) : chunk.copy(ctx, archive);
-					if (strcmp(name, "ARCADE.PAL") == 0 && chunk.isSection("VGA:")) {
-						stream->read(palette, 256*3);
+				ext = name;
+				while (*ext) {
+					if (*ext == '.') {
+						ext++;
+						break;
 					}
-					if (strcmp(name, "BGND.SCR")) {
-/*
-						if (chunk.isSection("BIN:"))
-							stream->read(binData, stream->size());
-						else if (chunk.isSection("VGA:"))
-							stream->read(vgaData, stream->size());
-*/
-					}
-
-					if (strcmp(ext, "GDS") == 0) {
-						if (chunk.isSection("INF:")) {
-						    stream->hexdump(stream->size());
-						} else if (chunk.isSection("SDS:")) {
-						    stream->hexdump(stream->size());
-						}
-					}
-
-					if (strcmp(ext, "TTM") == 0) {
-						if (chunk.isSection("VER:")) {
-						    char version[5];
-						    stream->read(version, sizeof(version));
-						    debug("        %s", version);
-						} else if (chunk.isSection("PAG:")) {
-						    uint16 pages;
-						    pages = stream->readUint16LE();
-						    debug("        %u", pages);
-						} else if (chunk.isSection("TT3:")) {
-						    stream->hexdump(stream->size());
-						} else if (chunk.isSection("TAG:")) {
-							uint16 count;
-
-							count = stream->readUint16LE();
-							debug("        %u", count);
-							for (uint16 k=0; k<count; k++) {
-							    byte ch;
-							    uint16 idx;
-							    Common::String str;
-
-							    idx = stream->readUint16LE();
-							    while ((ch = stream->readByte()))
-								str += ch;
-							    debug("        %2u: %2u, \"%s\"", k, idx, str.c_str());
-							}
-						}
-					}
-
-					uint16 tcount;
-					uint16 *tw, *th;
-					uint32 *toffsets;
-
-					uint16 *mtx;
-					uint16 mw, mh;
-
-					if (strcmp(ext, "BMP") == 0) {
-						if (chunk.isSection("INF:")) {
-							tcount = stream->readUint16LE();
-							debug("        [%u] =", tcount);
-
-							tw = new uint16[tcount];
-							th = new uint16[tcount];
-							for (uint16 k=0; k<tcount; k++)
-								tw[k] = stream->readUint16LE();
-							for (uint16 k=0; k<tcount; k++)
-								th[k] = stream->readUint16LE();
-
-							uint32 sz = 0;
-							toffsets = new uint32[tcount];
-							for (uint16 k=0; k<tcount; k++) {
-								debug("        %ux%u @%u", tw[k], th[k], sz);
-								toffsets[k] = sz;
-								sz += tw[k]*th[k];
-							}
-							debug("        BIN|VGA: %u bytes", (sz+1)/2);
-						} else if (chunk.isSection("MTX:")) {
-							uint32 mcount;
-
-							mw = stream->readUint16LE();
-							mh = stream->readUint16LE();
-							mcount = uint32(mw)*mh;
-							debug("        %ux%u: %u bytes", mw, mh, mcount*2);
-
-							mtx = new uint16[mcount];
-							for (uint32 k=0; k<mcount; k++) {
-								uint16 tile;
-								tile = stream->readUint16LE();
-								mtx[k] = tile;
-//								debug("        %u", tile);
-							}
-						}
-					}
-
-					// DCORNERS.BMP, DICONS.BMP, HELICOP2.BMP, WALKAWAY.BMP, KARWALK.BMP, BLGREND.BMP, FLAMDEAD.BMP, W.BMP, ARCADE.BMP
-					// MTX: SCROLL.BMP (intro title), SCROLL2.BMP
-					if (strcmp(name, "SCROLL2.BMP") == 0) {
-						if (chunk.isSection("BIN:"))
-							stream->read(binData, stream->size());
-						else if (chunk.isSection("VGA:"))
-							stream->read(vgaData, stream->size());
-
-						_tcount = tcount;
-						_tw = tw;
-						_th = th;
-						_toffsets = toffsets;
-
-						_mtx = mtx;
-						_mw = mw;
-						_mh = mh;
-					}
-					delete stream;
+					ext++;
 				}
+
+				if (isCompact(ext)) {
+					if (strcmp(ext, "BMP") == 0) {
+					    archive.hexdump(64);
+					}
+					if (strcmp(ext, "RST") == 0) {
+					    archive.hexdump(64);
+					}
+					if (strcmp(ext, "INS") == 0) {
+						/* IFF-8SVX audio. */
+					}
+					if (strcmp(ext, "VIN") == 0) {
+					    archive.hexdump(inSize);
+					}
+					if (strcmp(ext, "AMG") == 0) {
+						Common::String line;
+						while (1) {
+							line = archive.readLine();
+							debug("\"%s\"", line.c_str());
+
+							ctx.bytesRead += line.size()+1;
+
+							if (ctx.bytesRead >= ctx.inSize)
+								break;
+						}
+					}
+				} else {
+					struct DgdsChunk chunk;
+					int ret;
+					while ((ret = chunk.readHeader(ctx, archive, name)) != 0) {
+						Common::SeekableReadStream *stream;
+
+						if (ret == 1) {
+							break;
+						} else if (ret == 2) {
+							bool packed = chunk.isPacked(ext);
+							stream = packed ? chunk.decode(ctx, archive) : chunk.copy(ctx, archive);
+						}
+
+						if (strcmp(name, "DYNAMIX.PAL") == 0 && chunk.isSection("VGA:")) {
+							stream->read(palette, 256*3);
+						}
+						if (strcmp(name, "DYNAMIX.SCR"/*"BGND.SCR"*/) == 0) {
+							if (chunk.isSection("BIN:")) {
+								stream->read(binData, stream->size());
+							} else if (chunk.isSection("VGA:")) {
+								stream->read(vgaData, stream->size());
+							}
+						}
+
+						if (strcmp(ext, "ADS") == 0 || strcmp(ext, "ADH") == 0) {
+							if (chunk.isSection("SCR:")) {
+								stream->hexdump(stream->size());
+							}
+						}
+
+						if (strcmp(ext, "SX") == 0) {
+							if (chunk.isSection("INF:")) {
+								stream->hexdump(stream->size());
+							} else if (chunk.isSection("TAG:")) {
+								stream->hexdump(stream->size());
+							} else if (chunk.isSection("FNM:")) {
+								stream->hexdump(stream->size());
+							} else if (chunk.isSection("DAT:")) {
+								stream->hexdump(stream->size());
+
+								Common::DumpFile out;
+								char *buf;
+
+								uint sz;
+								sz = stream->size();
+								buf = new char[inSize];
+
+								if (!out.open(name)) {
+									debug("Couldn't write to %s", name);
+								} else {
+									stream->read(buf, inSize);
+									out.write(buf, inSize);
+									out.close();
+								}
+								delete [] buf;
+							}
+						}
+
+						if (strcmp(ext, "GDS") == 0) {
+							if (chunk.isSection("INF:")) {
+								stream->hexdump(stream->size());
+							} else if (chunk.isSection("SDS:")) {
+								stream->hexdump(stream->size());
+							}
+						}
+
+						if (strcmp(ext, "TTM") == 0) {
+							if (chunk.isSection("VER:")) {
+								char version[5];
+								stream->read(version, sizeof(version));
+								debug("        %s", version);
+							} else if (chunk.isSection("PAG:")) {
+								uint16 pages;
+								pages = stream->readUint16LE();
+								debug("        %u", pages);
+							} else if (chunk.isSection("TT3:")) {
+								stream->hexdump(stream->size());
+							} else if (chunk.isSection("TAG:")) {
+								uint16 count;
+
+								count = stream->readUint16LE();
+								debug("        %u", count);
+								for (uint16 k=0; k<count; k++) {
+									byte ch;
+									uint16 idx;
+									Common::String str;
+
+									idx = stream->readUint16LE();
+									while ((ch = stream->readByte()))
+										str += ch;
+									debug("        %2u: %2u, \"%s\"", k, idx, str.c_str());
+								}
+							}
+						}
+
+						uint16 tcount;
+						uint16 *tw, *th;
+						uint32 *toffsets;
+
+						uint16 *mtx;
+						uint16 mw, mh;
+
+						if (strcmp(ext, "BMP") == 0) {
+							if (chunk.isSection("INF:")) {
+								tcount = stream->readUint16LE();
+								debug("        [%u] =", tcount);
+
+								tw = new uint16[tcount];
+								th = new uint16[tcount];
+								for (uint16 k=0; k<tcount; k++)
+									tw[k] = stream->readUint16LE();
+								for (uint16 k=0; k<tcount; k++)
+									th[k] = stream->readUint16LE();
+
+								uint32 sz = 0;
+								toffsets = new uint32[tcount];
+								for (uint16 k=0; k<tcount; k++) {
+									debug("        %ux%u @%u", tw[k], th[k], sz);
+									toffsets[k] = sz;
+									sz += tw[k]*th[k];
+								}
+								debug("        BIN|VGA: %u bytes", (sz+1)/2);
+							} else if (chunk.isSection("MTX:")) {
+								uint32 mcount;
+
+								mw = stream->readUint16LE();
+								mh = stream->readUint16LE();
+								mcount = uint32(mw)*mh;
+								debug("        %ux%u: %u bytes", mw, mh, mcount*2);
+
+								mtx = new uint16[mcount];
+								for (uint32 k=0; k<mcount; k++) {
+									uint16 tile;
+									tile = stream->readUint16LE();
+									mtx[k] = tile;
+									//								debug("        %u", tile);
+								}
+							}
+						}
+
+						// DCORNERS.BMP, DICONS.BMP, HELICOP2.BMP, WALKAWAY.BMP, KARWALK.BMP, BLGREND.BMP, FLAMDEAD.BMP, W.BMP, ARCADE.BMP
+						// MTX: SCROLL.BMP (intro title), SCROLL2.BMP
+						if (strcmp(name, "XXXDYNAMIX.BMP") == 0) {
+							/*
+							if (chunk.isSection("BIN:"))
+								stream->read(binData, stream->size());
+							else if (chunk.isSection("VGA:"))
+								stream->read(vgaData, stream->size());
+
+							_tcount = tcount;
+							_tw = tw;
+							_th = th;
+							_toffsets = toffsets;
+
+							_mtx = mtx;
+							_mw = mw;
+							_mh = mh;
+							*/
+						}
+						delete stream;
+					}
+				}
+
 				debug("  [%u:%u] --", ctx.bytesRead, ctx.outSize);
 
 				archive.seek(offset + 13 + 4);
@@ -488,7 +590,8 @@ Common::Error DgdsEngine::run() {
 	debug("DgdsEngine::init");
 
 	// Rise of the Dragon.
-	explode("volume.vga"/*vga,rmf*/, true);
+	explode("volume.rmf"/*vga,rmf*/, true);
+//	return Common::kNoError;
 	for (uint i=0; i<256*3; i+=3) {
 		palette[i+0] <<= 2;
 		palette[i+1] <<= 2;
@@ -523,6 +626,20 @@ Common::Error DgdsEngine::run() {
 				}
 			}
 		}
+
+		// SCR:BIN|VGA viewer.
+		w = 320; h = 200;
+		vgaData_ = vgaData;
+		binData_ = binData;
+
+		for (uint i=0; i<w*h; i+=2) {
+			imgData[i+0]  = ((vgaData_[i/2] & 0xF0)     );
+			imgData[i+0] |= ((binData_[i/2] & 0xF0) >> 4);
+			imgData[i+1]  = ((vgaData_[i/2] & 0x0F) << 4);
+			imgData[i+1] |= ((binData_[i/2] & 0x0F)     );
+		}
+		g_system->fillScreen(0);
+		g_system->copyRectToScreen(imgData, w, 0, 0, w, h);
 /*
 		// BMP:INF|BIN|VGA browser.
 		w = _tw[k]; h = _th[k];
@@ -537,7 +654,7 @@ Common::Error DgdsEngine::run() {
 		}
 		//g_system->fillScreen(0);
 		g_system->copyRectToScreen(imgData, w, 0, 0, w, h);*/
-
+/*
 		// BMP:INF|BIN|VGA|MTX browser.
 		uint cx, cy;
 		cx = cy = 0;
@@ -564,7 +681,7 @@ Common::Error DgdsEngine::run() {
 			cy = 0;
 			cx += w;
 		}
-
+*/
 		g_system->updateScreen();
 		g_system->delayMillis(20);
 	}
